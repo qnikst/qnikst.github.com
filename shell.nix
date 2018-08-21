7f36c03ff6496afd21cd9705569f8eaba7e4e14e
Nicolas Mattia <nicolas@nmattia.com>
2018-08-21 11:02:17 +0300

    Fix package name

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
 let
   pkgs = import ./.;
+  ROOT = builtins.toString ./.;
 in pkgs.haskellPackages.shellFor {
-  packages=p: [p.qnikst-blog];
+  packages=p: [p.qnikst-github-com];
   shellHook=
     ''
-    alias hello=echo "hello"
+      alias hello=echo "hello"
+
+      foo_build() {
+        pushd ${ROOT}
+        cabal new-build
+        popd
+      }
     '';
+  buildInputs = [ pkgs.cabal-install ];
   }
